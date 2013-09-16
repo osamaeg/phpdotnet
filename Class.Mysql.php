27d0990f7cb6b2e0c76afd52b5bf2ef617b8ea01
osamaeg <developer_egypt@yahoo.com>
2013-09-16 19:11:27 +0200

    Update Class.Mysql.php

--- Class.Mysql.php
+++ Class.Mysql.php
@@ -12,6 +12,55 @@ define("__EmailAdmin__","osama_eg@live.com");
 @CopyRight 2010
 */
 class Mysql {
+//@var  address string Redirect Url
+protected $address;
+
+//@var  Error Boolean Check Error true
+protected $Error=true;
+
+//@var link Resource mysql
+protected $link;
+
+//@var Host,UserDb,PassDb,Dbname String  Paramters Connection.
+private $Host,$UserDb,$PassDb,$Dbname;
+
+//@var filetowrite,filemod,filetoread,filecontent  dirs String  Paramters hander File.
+private $filetowrite,$filemod,$filetoread,$filecontent,$dirs;
+
+//@var pattern String  Genrate key.
+protected  $pattern="0123456789abcdefghijklmonpqrstyvwxyz!@%&()*/-";
+
+//@var	string the table perfix
+protected $perfix='cm_';
+
+//@var	sql string the sql statment
+private $sql;
+
+
+//@var	table string Db Table
+protected $table;
+
+//@var	where string Sql Condtion.
+protected $where;
+
+//@var	SessionVar  Destroy variable Session.
+protected $SessionVar;
+
+
+//@var	Uploader  uploader file array
+protected $Uploader;
+
+//@var	numrows  count record
+protected $numrows='0';
+
+//@var	limit   record
+public $limit=5;
+
+//@var	start  record  
+public $start='0';
+
+public $condtions;
+
 	
 	
 	
