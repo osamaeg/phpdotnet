b04277b7bbc7f2b0be081fccbc706b0a94cd4559
osama salama <osama_eg@outlook.com>
2013-09-16 19:40:28 +0200

    Updated Class & add Error Function

--- Class.Mysql.php
+++ Class.Mysql.php
@@ -107,6 +107,105 @@ return $this->link;
 
 
 
+   /**
+ *Mysql::print_error() function Error Show
+ *@param sql String Error Mysql
+ *@return String
+*/
+
+
+
+public  function print_error($err = "") {
+
+$_error  = __MYSQLERROR_ERROR__." : ".mysql_error()."\n";
+$_error .= __MYSQLERROR_NUMBER__.": ".mysql_errno()."\n";
+$_error .= "Date: ".date("l dS of F Y h:i:s A")."\n";
+if($err != '') {
+$_error .= "$err";
+}
+$_error .= "\n---------------Query---------------";
+$output = "<html dir=rtl><head><meta http-equiv=Content-Type content=text/html; charset=utf-8>
+<title>Arab Portal Database Error</title>
+<style>P,BODY{ font-family:Windows UI,arial; font-size:12px; }</style>
+</head>
+<body>
+<br><br><blockquote><b>لم يكن قادر على اجراء الاستعلام من قاعدة البينات.</b><br>
+يرجى المحاولة بتحديث الصفحة باضغط على هذا الرابط <a href=\"javascript:window.location=window.location;\">اضغط هنا</a>
+اذا لم يجري الاستعلام الرجاء ابلاغ ادارة الموقع  <a href='mailto:".__EmailAdmin__."?subject=خطأ في قاعدة البينات'>مراسلة الموقع </a>
+<br><br><b>الاستعلام الذي تسبب في الخطأ</b><br>
+<form name='mysql'><textarea dir=\"ltr\" rows=\"15\" cols=\"70\">".$_error."\n$err</textarea></form></blockquote><br>
+<p><b><a href=\"$_SERVER[HTTP_HOST]/\" target=\"_blank\">Powerd By Osama salama</a></b></p></body></html>";
+print $output;
+exit;
+}
+
+
+
+
+
+   /**
+ *Mysql::Err() function Error Show
+ *@param sql String Error Mysql
+ *@return String
+*/
+
+
+
+
+public function Err($sql='') {
+		$err = "<font color='red'>
+				Mysql Error Occurred<br />
+				Error Details:<br />
+				File Name: ".__FILE__."<br />
+				Line Number: ". __LINE__."<br />
+				Err Number: ".mysql_errno()."<br />
+				Err Desc: ".mysql_error()."<br /></font>";
+		if($sql != '') {
+			$err .= "Query Says: <textarea cols='60' rows='8'>$sql</textarea>";
+		}
+		if ($this->Error) {
+			die($err);
+		}
+	}
+ 
+ 
+ 
+ /**
+  *Mysql::__destruct() function To Close Database 
+  *@return boolean True
+  */
+
+ public function __destruct(){
+ 
+ return @mysql_close($this->link);
+ 
+ }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
