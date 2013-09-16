3ed1ce25b5aa95c4badcee88101af550b0d79eec
osama salama <osama_eg@outlook.com>
2013-09-16 19:05:30 +0200

    Added Class Mysql

---
+++ Class.Mysql.php
@@ -0,0 +1,22 @@
+<?php
+define("__MYSQLERROR_ERROR__","Error");
+define("__MYSQLERROR_NUMBER__","Error No");
+define("__EmailAdmin__","osama_eg@live.com");
+
+
+/*
+@desc class Mysql to manage database  
+@Author Osama salaam
+@Package Mysql
+@Version 0.1
+@CopyRight 2010
+*/
+class Mysql {
+	
+	
+	
+	}
+
+
+
+?>
